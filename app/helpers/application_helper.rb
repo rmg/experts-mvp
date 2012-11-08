8c45d0521269535b35bd837a9e75e869227cc594
Ryan Graham <r.m.graham@gmail.com>
2012-11-07 21:05:34 -0800

    One last coat of paint..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def status_class(status, style='badge')
+    "#{style} #{style}" <<
+    case status
+    when 'here'
+      '-success'
+    when 'lunch'
+      '-warning'
+    when 'away'
+      ''
+    else
+      '-inverse'
+    end
+  end
+
+  def status_badge(status)
+    status = "away" if status.blank?
+    content_tag :span, status, class: 'big ' + status_class(status, 'badge')
+  end
 end
